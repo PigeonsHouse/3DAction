a67240b3531dfc1e5c497b9b5538c6ebd22a0488
PigeonsHouse <bullbull1230@gmail.com>
2022-05-14 17:45:01 +0900

    [add] ダッシュの追加と，回避のためのステート作成

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,10 +13,23 @@ public class Player : MonoBehaviour {
     public float limit;
     // カメラの高さ
     private float cameraHeight;
+    // プレイヤー→カメラのベクトル
+    private Vector3 cameraVec;
     // カメラ
     private GameObject mainCamera;
+    // プレイヤーの状態
+    private PlayerState playerState;
     
+    // 回避時間
+    public float avoidanceTime = 0.5f;
+    private float remainingAvoidanceTime;
+
     private Rigidbody m_rigidbody;
+
+    enum PlayerState {
+        move,
+        avoidance,
+    }
 
     void Start() {
         // マウスの固定と非表示
@@ -26,15 +39,22 @@ public class Player : MonoBehaviour {
         // カメラオブジェクトの取得
         mainCamera = GameObject.FindGameObjectsWithTag("MainCamera")[0];
         // カメラの初期位置設定
+        cameraVec = -transform.forward;
         mainCamera.transform.position = transform.position - cameraDistance * transform.forward;
         mainCamera.transform.LookAt(transform.position);
         
         // コンポーネントの取得
         m_rigidbody = GetComponent<Rigidbody>();
+
+        // ステート登録
+        playerState = PlayerState.move;
     }
 
     void Update() {
-        Move();
+        if (playerState == PlayerState.move) {
+            Move();
+        }
+        Avoidance();
         CalcCamera();
     }
 
@@ -43,6 +63,7 @@ public class Player : MonoBehaviour {
         float ver = Input.GetAxis("Vertical");
         // 左右(AD)の大きさ取得
         float hor = Input.GetAxis("Horizontal");
+        bool isShift = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
 
         if (hor != 0 || ver != 0) {
             // カメラに対して垂直，平行なベクトルの取得
@@ -51,7 +72,7 @@ public class Player : MonoBehaviour {
             Vector3 sideVec = new Vector3(forwardVec.z, 0, -forwardVec.x);
 
             // 移動ベクトルの作成
-            Vector3 moveVector = (ver * forwardVec + hor * sideVec) * Time.deltaTime * moveSpeed;
+            Vector3 moveVector = (ver * forwardVec + hor * sideVec) * Time.deltaTime * moveSpeed * (isShift ? 1.5f : 1);
             transform.LookAt(transform.position + moveVector);
 
             // 上下の速度の保持
@@ -60,24 +81,39 @@ public class Player : MonoBehaviour {
         }
     }
 
+    void Avoidance() {
+        if (playerState == PlayerState.move && Input.GetKeyDown("space")) {
+            playerState = PlayerState.avoidance;
+            remainingAvoidanceTime = avoidanceTime;
+        }else if (playerState == PlayerState.avoidance) {
+            if (remainingAvoidanceTime > 0) {
+                float moveVectorMagnitude = moveSpeed / (2.5f * (10f - 19 * remainingAvoidanceTime));
+                Vector3 moveVector = moveVectorMagnitude * transform.forward;
+                moveVector.y = m_rigidbody.velocity.y;
+                m_rigidbody.velocity = moveVector;
+                remainingAvoidanceTime -= Time.deltaTime;
+            } else {
+                m_rigidbody.velocity = Vector3.zero;
+                playerState = PlayerState.move;
+            }
+        }
+    }
+
     void CalcCamera() {
 		float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
 		float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
 
         // 左右回転の量の計算
-        Vector3 deferredPosition = (mainCamera.transform.position - transform.position);
-        deferredPosition.y = 0;
-        deferredPosition = deferredPosition.normalized;
-        deferredPosition = Quaternion.Euler(0, 5000f * Time.deltaTime * mouseX, 0) * deferredPosition;
+        cameraVec = Quaternion.Euler(0, 5000f * Time.deltaTime * mouseX, 0) * cameraVec;
 
         // 上下回転の量の計算
-        cameraHeight += mouseY;
+        cameraHeight -= mouseY;
         if (Mathf.Abs(cameraHeight) > limit) {
             cameraHeight = limit * Mathf.Sign(cameraHeight);
         }
 
         // カメラの位置決定
-        Vector3 addCameraVec = deferredPosition + cameraHeight * transform.up;
+        Vector3 addCameraVec = cameraVec + cameraHeight * transform.up;
         mainCamera.transform.position = transform.position + addCameraVec.normalized * cameraDistance;
         mainCamera.transform.LookAt(transform.position);
     }
